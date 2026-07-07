ab24f20f4dd9ddf3b2aaf460080507838ca82be8
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2026-07-07 10:27:59 -0600

    reorganized application
    currently doens't work - requires more organization

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.js");
+
 const nextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true, // !! Improtant for static export - allows you to go directly to url/pageRoute istead of url and them localy to pageRoute
   images: {
-    domains: ['d2qcvmovr4fv.cloudfront.net']
-  },
+      domains: ["d2qcvmovr4fv.cloudfront.net"]
+  }
 };
-export default nextConfig;
+
+export default withNextIntl(nextConfig);
---
+++ proxy.js
@@ -0,0 +1,10 @@
+// middleware.js
+
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./src/i18n/routing";
+
+export default createMiddleware(routing);
+
+export const config = {
+    matcher: ["/", "/(en|fr)/:path*"]
+};
